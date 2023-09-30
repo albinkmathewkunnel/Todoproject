b7045ff6b816ebb15ae54fc78443c6a157866ffe
albinkmathewkunnel <93862923+CIELOARCTURES@users.noreply.github.com>
2023-09-30 11:05:23 +0530

    added the mongoose globally

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
